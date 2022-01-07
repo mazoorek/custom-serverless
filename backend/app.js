e709ffb8c930cf6c92ddda67437a6de098477a63
pmazurek <piotr.mazurek98@gmail.com>
2022-01-07 01:28:29 +0100

    image wip

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const k8s = require('@kubernetes/client-node');
-
+const express = require('express');
 const dotenv = require('dotenv');
-dotenv.config({ path: './.env' });
 
+dotenv.config({ path: './.env' });
 const kc = new k8s.KubeConfig();
+const PORT = 8080;
+const HOST = '0.0.0.0';
 
 const cluster = {
     name: 'k8s-server',
@@ -17,9 +19,62 @@ const user = {
 
 kc.loadFromClusterAndUser(cluster, user);
 
-const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+const k8sCoreV1Api = kc.makeApiClient(k8s.CoreV1Api);
+const k8sNetworkingV1Api = kc.makeApiClient(k8s.NetworkingV1Api);
 
-k8sApi.listNamespacedPod('default').then((res) => {
-    console.log(res.body);
+const app = express();
+
+app.use(express.json({limit: '10kb'}));
+
+app.get('/pods', async (req, res) => {
+    let pods = await k8sCoreV1Api.listNamespacedPod('custom-serverless-apps').catch(e => console.log(e));
+    res.json(pods);
+});
+
+app.get('/pods-default', async (req, res) => {
+    let pods = await k8sCoreV1Api.listNamespacedPod('default').catch(e => console.log(e));
+    res.json(pods);
+});
+
+app.get('/pods-dev', async (req, res) => {
+    let pods = await k8sCoreV1Api.listNamespacedPod('dev').catch(e => console.log(e));
+    res.json(pods);
+});
+
+app.get('/ingresses', async (req, res) => {
+    let ingresses = await k8sNetworkingV1Api.listNamespacedIngress('custom-serverless-apps', true).catch(e => console.log(e));
+    res.json(ingresses);
+});
+
+app.get('/ingresses-default', async (req, res) => {
+    let ingresses = await k8sNetworkingV1Api.listNamespacedIngress('default', true).catch(e => console.log(e));
+    res.json(ingresses);
+});
+
+// app.post('/create', async (req, res) => {
+//     await k8sNetworkingV1Api.createNamespacedIngress('default', {
+//         apiVersions: 'networking.k8s.io/v1beta1',
+//         kind: 'Ingress',
+//         metadata: { name: `production-custom-${clientIdentifier}` },
+//         spec: {
+//             rules: [{
+//                 host: `${clientIdentifier}.example.com`,
+//                 http: {
+//                     paths: [{
+//                         backend: {
+//                             serviceName: 'production-auto-deploy',
+//                             servicePort: 5000
+//                         },
+//                         path: '/'
+//                     }]
+//                 }
+//             }],
+//             tls: [{ hosts: [`${clientIdentifier}.example.com`] }]
+//         }
+//     }).catch(e => console.log(e))
+// });
+
+app.listen(PORT, HOST,  () => {
+    console.log(`Running on port ${PORT}`);
 });
 
